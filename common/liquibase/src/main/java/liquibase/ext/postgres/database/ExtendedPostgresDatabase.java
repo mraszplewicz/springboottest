0e248755f31c901787d4a09a2b168e70d6a542d6
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2015-03-01 22:45:20 +0100

    fixed nondeterministic behavior of liquibaseUpdateSQL

--- common/liquibase/src/main/java/liquibase/ext/postgres/database/ExtendedPostgresDatabase.java
+++ common/liquibase/src/main/java/liquibase/ext/postgres/database/ExtendedPostgresDatabase.java
@@ -6,6 +6,11 @@ import liquibase.structure.DatabaseObject;
 public class ExtendedPostgresDatabase extends liquibase.database.core.PostgresDatabase {
 
     @Override
+    public int getPriority() {
+        return 2;
+    }
+
+    @Override
     public String escapeObjectName(String objectName, Class<? extends DatabaseObject> objectType) {
         if (objectName != null) {
             objectName = objectName.trim();
