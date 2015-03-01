9269f2357c6af4fe2359874eb26560ff4a188fdd
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2015-03-01 17:44:48 +0100

    resolved problem with quotes and postgres

---
+++ common/liquibase/src/main/java/liquibase/ext/postgres/database/ExtendedPostgresDatabase.java
@@ -0,0 +1,22 @@
+package liquibase.ext.postgres.database;
+
+import liquibase.database.ObjectQuotingStrategy;
+import liquibase.structure.DatabaseObject;
+
+public class ExtendedPostgresDatabase extends liquibase.database.core.PostgresDatabase {
+
+    @Override
+    public String escapeObjectName(String objectName, Class<? extends DatabaseObject> objectType) {
+        if (objectName != null) {
+            objectName = objectName.trim();
+            if (mustQuoteObjectName(objectName, objectType)) {
+                return quoteObject(objectName, objectType);
+            } else if (quotingStrategy == ObjectQuotingStrategy.QUOTE_ALL_OBJECTS) {
+                return quoteObject(objectName, objectType);
+            }
+            objectName = objectName.trim();
+        }
+        return objectName;
+    }
+
+}
