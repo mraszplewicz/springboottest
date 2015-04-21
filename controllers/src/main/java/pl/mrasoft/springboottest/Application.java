4e6ca5178bb754c24920b0cc5ede50587812d692
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2015-04-21 03:11:26 +0200

    jboss deployment

--- controllers/src/main/java/pl/mrasoft/springboottest/Application.java
+++ controllers/src/main/java/pl/mrasoft/springboottest/Application.java
@@ -2,13 +2,11 @@ package pl.mrasoft.springboottest;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -19,8 +17,7 @@ import pl.mrasoft.springboottest.model.Wasteland2Weapon;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- tomcatwar/src/main/java/pl/mrasoft/springboottest/ApplicationWebXml.java
+++ tomcatwar/src/main/java/pl/mrasoft/springboottest/ApplicationWebXml.java
@@ -1,13 +1,27 @@
 package pl.mrasoft.springboottest;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class ApplicationWebXml extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        super.onStartup(container);
+
+        ServletRegistration.Dynamic registration = (ServletRegistration.Dynamic) container.getServletRegistration(EmbeddedWebApplicationContext.DISPATCHER_SERVLET_NAME);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/*");
     }
 
 }
